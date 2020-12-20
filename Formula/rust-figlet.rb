40003c8accf4233b65e7b073cb4a533e03034cc0
Shruthik Musukula <shruthik.musukula@gmail.com>
2020-12-19 21:50:07 -0800

    First release

---
+++ Formula/rust-figlet.rb
@@ -0,0 +1,14 @@
+#Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Rust-Figlet < Formula
+  desc "Cross-platform Figlet Text Displayer Constructed Using Rust"
+  homepage "https://github.com/shruthikmusukula/rust_figlet"
+  url "https://github.com/shruthikmusukula/rust_figlet/releases/download/v0.1.0/rust-figlet.tar.gz"
+  sha256 "cadb1b4a503046da7fe5e89adea442e8e163dc337687fc2317894fe6fb07f488"
+  version "0.1.0"
+
+  def install
+    bin.install "rust-figlet"
+  end
+end
